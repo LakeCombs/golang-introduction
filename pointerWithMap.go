7401828d1a0b466d959fa115cfea6740bc1097e1
LakeCombs <adesolaolamilekan2@gmail.com>
2021-10-19 11:57:27 +0100

    pointers

--- pointerWithMap.go
+++ pointerWithMap.go
@@ -6,6 +6,8 @@ func main() {
 	m := map[string]int{"one": 1, "two": 2, "three": 3 }
 
 	for k := range m {
-fmt.Printf("keys: %q \n " , k)
-}
+	fmt.Printf("keys: %q \n " , k)
+	fmt.Printf("keys: %s\n " , k)
+	//%q use double quote while %S uses the single quote
+	}
 }
